6a68fdef0bcbedc6a55b83610dc69087cd54322b
perviz085 <perviz.aydinli@gmail.com>
2023-10-26 14:44:24 +0400

    yaaaa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel  ccc         </title>
+        <title>bu nedir yaaaaa</title>
 
         <!-- Fonts -->
         <link rel="preconnect"      href="https://fonts.bunny.net">
